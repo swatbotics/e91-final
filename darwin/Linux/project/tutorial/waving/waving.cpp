28b83925a6d604fcd68d72b8ac04a749ab4c4955
DaRwIN-OP <darwin@darwin-d2.(none)>
2013-05-16 15:20:47 -0400

    done with waving for now

--- darwin/Linux/project/tutorial/waving/waving.cpp
+++ darwin/Linux/project/tutorial/waving/waving.cpp
@@ -22,7 +22,6 @@
 
 using namespace Robot;
 
-
 int motion_initialization(){
   //////////////////// Framework Initialize ////////////////////////////
   LinuxCM730* linux_cm730 = new LinuxCM730("/dev/ttyUSB0");
@@ -36,17 +35,26 @@ int motion_initialization(){
   LinuxMotionTimer *motion_timer = new LinuxMotionTimer(MotionManager::GetInstance());
   motion_timer->Start();
 
+  std::cout << "before\n";
+
+  sleep(1);
+
+  std::cout << "after\n";
+
   MotionManager::GetInstance()->SetEnable(true);
   for (int i=1; i<=20; i++){
+    std::cout << "Joint " << i << " at " << Body::GetInstance()->m_Joint.GetAngle(i) << "\n";
+    Body::GetInstance()->m_Joint.SetAngle(i, Body::GetInstance()->m_Joint.GetAngle(i));
     Body::GetInstance()->m_Joint.SetEnable(i,true,true);
-    Body::GetInstance()->m_Joint.SetAngle(i,0);
   }
 
   return 0;
   
 }
 
-
+double lerp(int a, int b, double t){
+  return a*(1-t)+b*t;
+}
 
 double getTimeAsDouble() {
   struct timeval tp;
@@ -55,53 +63,32 @@ double getTimeAsDouble() {
 }
 
 int main(){
-  /*
- LinuxCM730 linux_cm730("/dev/ttyUSB0");
-  CM730 cm730(&linux_cm730);
-  if(MotionManager::GetInstance()->Initialize(&cm730) == false)
-  {
-    std::cout<<"Fail to initialize Motion Manager!\n";
-    return -1;
-  }
-  MotionManager::GetInstance()->AddModule((MotionModule*)Body::GetInstance());	
-  LinuxMotionTimer *motion_timer = new LinuxMotionTimer(MotionManager::GetInstance());
-
-  std::cout << "Made timer\n";
-  motion_timer->Start();
-  std::cout << "Did timer\n";
-
-  MotionManager::GetInstance()->SetEnable(true);
-  for (int i=1; i<=20; i++){
-    Body::GetInstance()->m_Joint.SetEnable(i,true,true);
-    Body::GetInstance()->m_Joint.SetAngle(i,0);
-  }
-
-  std::cout << "About to loop\n";
-  */
   if (motion_initialization()!=0){
 		std::cout<<"Shit went down"<<std::endl;
 		return 0;
 	}
 
-  double start = getTimeAsDouble();
-
+  double start = getTimeAsDouble(); 
+  double init_angle[21];
+  for (int i=1;i<21;i++){
+    init_angle[i] = Body::GetInstance()->m_Joint.GetAngle(i);
+  }
   while (1) {
     double t = getTimeAsDouble()-start;
     double sin_val = 20*sin(2*(t));
     for (int i=1; i<20; ++i) {
-      std::cout << "Joint " << i << " at " << Body::GetInstance()->m_Joint.GetAngle(i) << " at time " << (getTimeAsDouble()-start) << "\n";
+      //      std::cout << "Joint " << i << " at " << Body::GetInstance()->m_Joint.GetAngle(i) << " at time " << (getTimeAsDouble()-start) << "\n";
     }
-    double amp = std::min(4*t, 1.0);
-
-    Body::GetInstance()->m_Joint.SetAngle(4,-45);
-    Body::GetInstance()->m_Joint.SetAngle(2,-90);
-    Body::GetInstance()->m_Joint.SetAngle(12,90);
+    double amp = std::min(.01*t, 1.0);
+    double a = lerp(0, 1, amp);
+    Body::GetInstance()->m_Joint.SetAngle(4,init_angle[4]+a*(-45-init_angle[4]));
+    Body::GetInstance()->m_Joint.SetAngle(2,init_angle[2]+a*(-90-init_angle[2]));
+    Body::GetInstance()->m_Joint.SetAngle(12,init_angle[12]+a*(90-init_angle[12]));
     Body::GetInstance()->m_Joint.SetAngle(7,.5*sin_val);
     Body::GetInstance()->m_Joint.SetAngle(15, .5*sin_val);
 
     Body::GetInstance()->m_Joint.SetAngle(6,amp*sin_val);
-
-  };
+  }
 
   return 0;
 }
